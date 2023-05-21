e66b95720196a22c8780a89865e75c41d8c148b0
Miguel Correa <miguelc1221@gmail.com>
2023-05-20 23:27:26 -0600

    change wrapper height

--- app/components/pageWrapper/pageWrapper.tsx
+++ app/components/pageWrapper/pageWrapper.tsx
@@ -13,7 +13,7 @@ export const PageWrapper = ({
   return (
     <div
       className={clsx(
-        "mx-10 min-h-screen pb-[6.2rem] pt-[2.313rem] md:pt-[4.063rem] md:pb-[7.5rem]",
+        "mx-10 min-h-screen pb-[6.2rem] pt-[2.313rem] supports-[min-height:100cqh]:min-h-[100cqh] supports-[min-height:100svh]:min-h-[100svh] md:pt-[4.063rem] md:pb-[7.5rem]",
         className
       )}
       style={{ ...style }}
